59066a0a4525457d5e0c9464b774f8661aad048d
Christian Gabriana <142938707+christiangabr@users.noreply.github.com>
2023-11-07 14:10:31 -0600

    the customer table is now being updated for each transaction

--- hw2.js
+++ hw2.js
@@ -13,26 +13,189 @@ const creds = require('./creds.json');
 const pool = new Pool(creds);
 
 app.get('/', async (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Transactions</title>
+        </head>
+        <body>
+            <a href="/food_list"> <button>Food List Page</button> </a>
+            <br>
+            <br>
+            <a href="/customer"> <button> Customers </button> </a>
+        </body>
+        </html>
+    `);
+});
+
+
+app.get('/food_list', async (req, res) => {
+    const foodId = req.query.foodId;
+    const customerId = req.query.customerId;
+
+    if (foodId && customerId) {
+        try {
+            await pool.query('BEGIN');
+            const foodData = await pool.query('SELECT * FROM food_list WHERE food_id = $1', [foodId]);
+
+            if (foodData.rows.length > 0) {
+                const food = foodData.rows[0];
+                const transactionDate = new Date().toJSON().slice(0, 10); // Get the current date (YYYY-MM-DD)
+                const queryResult = await pool.query('INSERT INTO transactions (customer_id, food_id, transaction_date) VALUES ($1, $2, $3) RETURNING t_id', [customerId, food.food_id, transactionDate]);
+
+                // Add transaction
+                if (queryResult.rows.length > 0) {
+                    const t_id = queryResult.rows[0].t_id;
+                    await pool.query('UPDATE customer SET account_balance = account_balance - $1 WHERE customer_id = $2', [food.price, customerId]);
+                    await pool.query('COMMIT');
+                } else {
+                    await pool.query('ROLLBACK');
+                    return res.status(500).send("Failed to retrieve transaction ID.");
+                }
+                
+            } else {
+                await pool.query('ROLLBACK');
+                return res.status(404).send("Food not found.");
+            }
+        } catch (error) {
+            await pool.query('ROLLBACK');
+            return res.status(500).send("Transaction error: " + error.message);
+        }
+    }
+
+    let food_listHtml = '';
+    let food_listHtml2 = '';
+    let food_listHtml3 = '';
+    let food_listHtml4 = '';
+
+    try {
+        const result = await pool.query(`SELECT * FROM food_list WHERE food_id >= 100 AND food_id < 200 ORDER BY food_id`);
+        if (result.rows.length > 0) {
+            food_listHtml = result.rows.map(row => {
+                return `<p>FoodID: ${row.food_id}, Food: ${row.food_name}, Price: ${'$' + row.price} </p>`;
+            }).join('');
+        }
+
+        const result2 = await pool.query(`SELECT * FROM food_list WHERE food_id >= 200 AND food_id < 300 ORDER BY food_id`);
+        if (result2.rows.length > 0) {
+            food_listHtml2 = result2.rows.map(row => {
+                return `<p>FoodID: ${row.food_id}, Food: ${row.food_name}, Price: ${'$' + row.price} </p>`;
+            }).join('');
+        }
+
+        const result3 = await pool.query(`SELECT * FROM food_list WHERE food_id >= 300 AND food_id < 400 ORDER BY food_id`);
+        if (result3.rows.length > 0) {
+            food_listHtml3 = result3.rows.map(row => {
+                return `<p>FoodID: ${row.food_id}, Food: ${row.food_name}, Price: ${'$' + row.price} </p>`;
+            }).join('');
+        }
+
+        const result4 = await pool.query(`SELECT * FROM food_list WHERE food_id >= 400 AND food_id < 500 ORDER BY food_id`);
+        if (result4.rows.length > 0) {
+            food_listHtml4 = result4.rows.map(row => {
+                return `<p>FoodID: ${row.food_id}, Food: ${row.food_name}, Price: ${'$' + row.price} </p>`;
+            }).join('');
+        }
+    } catch (error) {
+        return res.status(500).send("Error: " + error.message);
+    }
+
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title> Food List </title>
+        </head>
+        <body>
+            <h2> Food List Page     
+            <a href="/customer"> <button> Customers </button> </a>
+            </h2> 
+            <h3> Chinese Restaurant </h3>
+            ${food_listHtml}
+            <h3> Italian Restaurant </h3>
+            ${food_listHtml2}
+            <h3> Mexican Restaurant </h3>
+            ${food_listHtml3}
+            <h3> Japanese Restaurant </h3>
+            ${food_listHtml4}
+            <div>
+                <form action="/food_list" method="GET">
+                    <label for="customerId">Enter Customer ID (1-10):</label>
+                    <input type="number" name="customerId" id="customerId" required>
+                    <label for="foodId">Enter Food ID:</label>
+                    <input type="number" name="foodId" id="foodId" required>
+                    <button type="submit">Buy Food</button>
+                </form>
+            <div>
+        </body>
+        </html>
+    `);
+});
+
+app.get('/customer', async (req, res) => {
+
+    try {
+        const result = await pool.query(`SELECT * FROM customer ORDER BY customer_id`);
+        if (result.rows.length > 0) {
+            customerName = result.rows[0].customer_name; 
+            customersHtml = result.rows.map(row => {
+                return `<p>customerID: ${row.customer_id}, First Name: ${row.first_name}, Last Name: ${row.last_name}, Age: ${row.age}, Account Balance: ${'$' + row.account_balance}</p>`;
+            }).join('');
+        }
+
+    } catch (error) {
+        return res.status(500).send("Error: " + error.message);
+    }
+
+    res.send(`
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title> Customers </title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Transactions</title>
+        </head>
+        <body>
+        <h2> Customers </h2>
+        ${customersHtml}
+            <form action="/transactions" method="GET">
+                <label for="customerId">Enter Customer ID:</label>
+                <input type="number" name="customerId" id="customerId" required>
+                <button type="submit">Get Transactions</button>
+            </form>
+        </body>
+        </html>
+    `);
+});
+
+app.get('/transactions', async (req, res) => {
     const customerId = req.query.customerId;
     let transactionsHtml = "";
-    let totalPrice = 0;
-    let customerName = "";
+    let totalSpent = 0;
+    let firstName = "";
+    let lastName = "";
 
     if (customerId) {
         try {
             const result = await pool.query(`
-                SELECT t.*, p.name AS product_name, p.price AS product_price, c.name AS customer_name 
+                SELECT t.*, f.food_name AS food_name, f.price AS food_price, c.first_name AS first_name, c.last_name as last_name, r.restaurant_name
                 FROM transactions t 
-                JOIN product p ON t.product_id = p.product_id 
+                JOIN food_list f ON t.food_id = f.food_id 
                 JOIN customer c ON t.customer_id = c.customer_id
+                JOIN restaurant r on f.restaurant_id = r.restaurant_id
                 WHERE t.customer_id = $1
             `, [customerId]);
     
             if (result.rows.length > 0) {
-                customerName = result.rows[0].customer_name; 
+                firstName = result.rows[0].first_name;
+                lastName = result.rows[0].last_name;
                 transactionsHtml = result.rows.map(row => {
-                    totalPrice += row.product_price;
-                    return `<p>ID: ${row.t_id}, Product: ${row.product_name}, Price: ${row.product_price}, Date: ${row.transaction_date}</p>`;
+                    totalSpent += row.food_price;
+                    return `<p>ID: ${row.t_id}, Restaurant: ${row.restaurant_name}, Food: ${row.food_name}, Price: ${'$' + row.food_price}, Date: ${row.transaction_date}</p>`;
                 }).join('');
             }
         } catch (err) {
@@ -49,119 +212,21 @@ app.get('/', async (req, res) => {
             <title>Transactions</title>
         </head>
         <body>
-            <form action="/" method="GET">
-                <label for="customerId">Enter Customer ID:</label>
-                <input type="number" name="customerId" id="customerId" required>
-                <button type="submit">Get Transactions</button>
-            </form>
-            ${customerName ? `<h2>customer.name = ${customerName}:</h2>` : '<h2>Enter a Customer ID (1-5) to view transactions.</h2>'}
+            ${firstName ? `<h2> Customer: ${firstName + " " + lastName} </h2>` : '<h2>Enter a Customer ID (1-10) to view transactions.</h2>'}
             <div>
                 <h3>Transactions:</h3>
                 ${transactionsHtml}
-                ${transactionsHtml ? `<p>Total Price: ${totalPrice}</p>` : ''}
+                ${transactionsHtml ? `<p>Total Spent: ${'$' + totalSpent}</p>` : ''}
             </div>
-            <a href="/products">Click here to go to products</a>
-            <a href="/employees">Click here to go to employees</a>
+            <a href="/food_list">
+            <button>Food List Page</button> </a>
+            <br>
+            <br>
+            <a href="/customer"> <button> Customers </button> </a>
         </body>
         </html>
     `);
 });
-
-app.get('/products', async (req, res) => {
-
-    const productId = req.query.productId;
-    if (productId){
-        try {
-            await pool.query('BEGIN');
-            await pool.query('UPDATE product SET quantity = quantity - 1 WHERE product_id = $1 AND quantity > 0', [productId]);
-            await pool.query('COMMIT');
-        } catch (error) {
-            await pool.query('ROLLBACK');
-            return res.status(500).send("Transaction error: " + error.message);
-        }
-    }
-    try {
-        const result = await pool.query(`SELECT * FROM PRODUCT order by name desc`);
-        if (result.rows.length > 0) {
-            customerName = result.rows[0].customer_name; 
-            productsHtml = result.rows.map(row => {
-                return `<p>Product: ${row.name},ProductID: ${row.product_id}, Price: ${row.price}, Quantity: ${row.quantity}</p>`;
-            }).join('');
-        }
-        
-
-    } catch (error) {
-        return res.status(500).send("Error: " + error.message);
-    }
-
-    res.send(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title> PRODUCTS</title>
-        </head>
-        <body>
-            <h2> Hey were at products page </h2>
-            ${productsHtml}
-            <div>
-                <form action="/products" method="GET">
-                    <label for="productId">Enter Product ID:</label>
-                    <input type="number" name="productId" id="productId" required>
-                    <button type="submit">Buy Product</button>
-                </form>
-            <div>
-        </body>
-        </html>
-    `);
-});
-
-app.get('/employees', async (req, res) => {
-
-    const first_name = req.query.first_name;
-    const last_name = req.query.last_name;
-    let employeesHtml = "";
-
-    try {
-        const result = await pool.query('SELECT * FROM EMPLOYEE WHERE first_name=$1 AND last_name=$2', [first_name,last_name]);
-        if (result.rows.length > 0) {
-            employeesHtml = result.rows.map(row => {
-                return `<p>EmployeeID: ${row.employee_id}, First Name: ${row.first_name},Last Name: ${row.last_name}, Age: ${row.age}</p>`;
-            }).join('');
-        }
-        
-
-    } catch (error) {
-        return res.status(500).send("Error: " + error.message);
-    }
-
-
-    res.send(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title> Employees</title>
-        </head>
-        <body>
-            <h1>HERE AT EMPLOYEES</h1>
-            ${employeesHtml}
-            <div>
-            <form action="/employees" method="GET">
-                <label for="first_name">Enter first_name:</label>
-                <input type="text" name="first_name" id="first_name" required>
-
-                <label for="last_name">Enter last_name:</label>
-                <input type="text" name="last_name" id="last_name" required>
-
-                <button type="submit">Submit</button>
-            </form>
-        <div>
-        </body>
-        </html>
-    `);
-});
-
-
-
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
